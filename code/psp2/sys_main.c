baecc39615e8873383747f72841adc150348250e
cpasjuste <cpasjuste@gmail.com>
2018-01-23 19:50:05 +0100

    minor changes

--- code/psp2/sys_main.c
+++ code/psp2/sys_main.c
@@ -36,8 +36,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #	include "SDL.h"
 #	include "SDL_cpuinfo.h"
 #else
+
 #	include <SDL.h>
 #	include <SDL_cpuinfo.h>
+#include <vitasdk.h>
+
 #endif
 #endif
 
@@ -47,8 +50,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../qcommon/q_shared.h"
 #include "../qcommon/qcommon.h"
 
-static char binaryPath[ MAX_OSPATH ] = { 0 };
-static char installPath[ MAX_OSPATH ] = { 0 };
+int _newlib_heap_size_user = 192 * 1024 * 1024;
+
+static char binaryPath[MAX_OSPATH] = {0};
+static char installPath[MAX_OSPATH] = {0};
 
 mode_t umask(mode_t mask) {
     return 0;
@@ -59,9 +64,8 @@ mode_t umask(mode_t mask) {
 Sys_SetBinaryPath
 =================
 */
-void Sys_SetBinaryPath(const char *path)
-{
-	Q_strncpyz(binaryPath, path, sizeof(binaryPath));
+void Sys_SetBinaryPath(const char *path) {
+    Q_strncpyz(binaryPath, path, sizeof(binaryPath));
 }
 
 /*
@@ -69,9 +73,8 @@ void Sys_SetBinaryPath(const char *path)
 Sys_BinaryPath
 =================
 */
-char *Sys_BinaryPath(void)
-{
-	return binaryPath;
+char *Sys_BinaryPath(void) {
+    return binaryPath;
 }
 
 /*
@@ -79,9 +82,8 @@ char *Sys_BinaryPath(void)
 Sys_SetDefaultInstallPath
 =================
 */
-void Sys_SetDefaultInstallPath(const char *path)
-{
-	Q_strncpyz(installPath, path, sizeof(installPath));
+void Sys_SetDefaultInstallPath(const char *path) {
+    Q_strncpyz(installPath, path, sizeof(installPath));
 }
 
 /*
@@ -89,12 +91,11 @@ void Sys_SetDefaultInstallPath(const char *path)
 Sys_DefaultInstallPath
 =================
 */
-char *Sys_DefaultInstallPath(void)
-{
-	if (*installPath)
-		return installPath;
-	else
-		return Sys_Cwd();
+char *Sys_DefaultInstallPath(void) {
+    if (*installPath)
+        return installPath;
+    else
+        return Sys_Cwd();
 }
 
 /*
@@ -102,9 +103,8 @@ char *Sys_DefaultInstallPath(void)
 Sys_DefaultAppPath
 =================
 */
-char *Sys_DefaultAppPath(void)
-{
-	return Sys_BinaryPath();
+char *Sys_DefaultAppPath(void) {
+    return Sys_BinaryPath();
 }
 
 /*
@@ -114,9 +114,8 @@ Sys_In_Restart_f
 Restart the input subsystem
 =================
 */
-void Sys_In_Restart_f( void )
-{
-	IN_Restart( );
+void Sys_In_Restart_f(void) {
+    IN_Restart();
 }
 
 /*
@@ -126,9 +125,8 @@ Sys_ConsoleInput
 Handle new console input
 =================
 */
-char *Sys_ConsoleInput(void)
-{
-	return CON_Input( );
+char *Sys_ConsoleInput(void) {
+    return CON_Input();
 }
 
 /*
@@ -136,138 +134,32 @@ char *Sys_ConsoleInput(void)
 Sys_GetClipboardData
 ==================
 */
-char *Sys_GetClipboardData(void)
-{
+char *Sys_GetClipboardData(void) {
 #ifdef DEDICATED
-	return NULL;
+    return NULL;
 #else
-	char *data = NULL;
-	char *cliptext;
+    char *data = NULL;
+    char *cliptext;
 
-	if ( ( cliptext = SDL_GetClipboardText() ) != NULL ) {
-		if ( cliptext[0] != '\0' ) {
-			size_t bufsize = strlen( cliptext ) + 1;
+    if ((cliptext = SDL_GetClipboardText()) != NULL) {
+        if (cliptext[0] != '\0') {
+            size_t bufsize = strlen(cliptext) + 1;
 
-			data = Z_Malloc( bufsize );
-			Q_strncpyz( data, cliptext, bufsize );
+            data = Z_Malloc(bufsize);
+            Q_strncpyz(data, cliptext, bufsize);
 
-			// find first listed char and set to '\0'
-			strtok( data, "\n\r\b" );
-		}
-		SDL_free( cliptext );
-	}
-	return data;
+            // find first listed char and set to '\0'
+            strtok(data, "\n\r\b");
+        }
+        SDL_free(cliptext);
+    }
+    return data;
 #endif
 }
 
-#ifdef DEDICATED
-#	define PID_FILENAME PRODUCT_NAME "_server.pid"
-#else
-#	define PID_FILENAME PRODUCT_NAME ".pid"
-#endif
+void Sys_InitPIDFile(const char *gamedir) {}
 
-/*
-=================
-Sys_PIDFileName
-=================
-*/
-static char *Sys_PIDFileName( const char *gamedir )
-{
-	const char *homePath = Cvar_VariableString( "fs_homepath" );
-
-	if( *homePath != '\0' )
-		return va( "%s/%s/%s", homePath, gamedir, PID_FILENAME );
-
-	return NULL;
-}
-
-/*
-=================
-Sys_RemovePIDFile
-=================
-*/
-void Sys_RemovePIDFile( const char *gamedir )
-{
-	char *pidFile = Sys_PIDFileName( gamedir );
-
-	if( pidFile != NULL )
-		remove( pidFile );
-}
-
-/*
-=================
-Sys_WritePIDFile
-
-Return qtrue if there is an existing stale PID file
-=================
-*/
-static qboolean Sys_WritePIDFile( const char *gamedir )
-{
-	char      *pidFile = Sys_PIDFileName( gamedir );
-	FILE      *f;
-	qboolean  stale = qfalse;
-
-	if( pidFile == NULL )
-		return qfalse;
-
-	// First, check if the pid file is already there
-	if( ( f = fopen( pidFile, "r" ) ) != NULL )
-	{
-		char  pidBuffer[ 64 ] = { 0 };
-		int   pid;
-
-		pid = fread( pidBuffer, sizeof( char ), sizeof( pidBuffer ) - 1, f );
-		fclose( f );
-
-		if(pid > 0)
-		{
-			pid = atoi( pidBuffer );
-			if( !Sys_PIDIsRunning( pid ) )
-				stale = qtrue;
-		}
-		else
-			stale = qtrue;
-	}
-
-	if( FS_CreatePath( pidFile ) ) {
-		return 0;
-	}
-
-	if( ( f = fopen( pidFile, "w" ) ) != NULL )
-	{
-		fprintf( f, "%d", Sys_PID( ) );
-		fclose( f );
-	}
-	else
-		Com_Printf( S_COLOR_YELLOW "Couldn't write %s.\n", pidFile );
-
-	return stale;
-}
-
-/*
-=================
-Sys_InitPIDFile
-=================
-*/
-void Sys_InitPIDFile( const char *gamedir ) {
-	if( Sys_WritePIDFile( gamedir ) ) {
-#ifndef DEDICATED
-		char message[1024];
-		char modName[MAX_OSPATH];
-
-		FS_GetModDescription( gamedir, modName, sizeof ( modName ) );
-		Q_CleanStr( modName );
-
-		Com_sprintf( message, sizeof (message), "The last time %s ran, "
-			"it didn't exit properly. This may be due to inappropriate video "
-			"settings. Would you like to start with \"safe\" video settings?", modName );
-
-		if( Sys_Dialog( DT_YES_NO, message, "Abnormal Exit" ) == DR_YES ) {
-			Cvar_Set( "com_abnormalExit", "1" );
-		}
-#endif
-	}
-}
+void Sys_RemovePIDFile(const char *gamedir) {}
 
 /*
 =================
@@ -276,25 +168,16 @@ Sys_Exit
 Single exit point (regular exit or in case of error)
 =================
 */
-static __attribute__ ((noreturn)) void Sys_Exit( int exitCode )
-{
-	CON_Shutdown( );
+static __attribute__ ((noreturn)) void Sys_Exit(int exitCode) {
+    CON_Shutdown();
 
-#ifndef DEDICATED
-	SDL_Quit( );
-#endif
+    SDL_Quit();
 
-	if( exitCode < 2 && com_fullyInitialized )
-	{
-		// Normal exit
-		Sys_RemovePIDFile( FS_GetCurrentGameDir() );
-	}
+    NET_Shutdown();
 
-	NET_Shutdown( );
+    Sys_PlatformExit();
 
-	Sys_PlatformExit( );
-
-	exit( exitCode );
+    exit(exitCode);
 }
 
 /*
@@ -302,9 +185,8 @@ static __attribute__ ((noreturn)) void Sys_Exit( int exitCode )
 Sys_Quit
 =================
 */
-void Sys_Quit( void )
-{
-	Sys_Exit( 0 );
+void Sys_Quit(void) {
+    Sys_Exit(0);
 }
 
 /*
@@ -312,20 +194,8 @@ void Sys_Quit( void )
 Sys_GetProcessorFeatures
 =================
 */
-cpuFeatures_t Sys_GetProcessorFeatures( void )
-{
-	cpuFeatures_t features = 0;
-
-#ifndef DEDICATED
-	if( SDL_HasRDTSC( ) )      features |= CF_RDTSC;
-	if( SDL_Has3DNow( ) )      features |= CF_3DNOW;
-	if( SDL_HasMMX( ) )        features |= CF_MMX;
-	if( SDL_HasSSE( ) )        features |= CF_SSE;
-	if( SDL_HasSSE2( ) )       features |= CF_SSE2;
-	if( SDL_HasAltiVec( ) )    features |= CF_ALTIVEC;
-#endif
-
-	return features;
+cpuFeatures_t Sys_GetProcessorFeatures(void) {
+    return (cpuFeatures_t) 0;
 }
 
 /*
@@ -333,80 +203,10 @@ cpuFeatures_t Sys_GetProcessorFeatures( void )
 Sys_Init
 =================
 */
-void Sys_Init(void)
-{
-	Cmd_AddCommand( "in_restart", Sys_In_Restart_f );
-	Cvar_Set( "arch", OS_STRING " " ARCH_STRING );
-	Cvar_Set( "username", Sys_GetCurrentUser( ) );
-}
-
-/*
-=================
-Sys_AnsiColorPrint
-
-Transform Q3 colour codes to ANSI escape sequences
-=================
-*/
-void Sys_AnsiColorPrint( const char *msg )
-{
-	static char buffer[ MAXPRINTMSG ];
-	int         length = 0;
-	static int  q3ToAnsi[ 8 ] =
-	{
-		30, // COLOR_BLACK
-		31, // COLOR_RED
-		32, // COLOR_GREEN
-		33, // COLOR_YELLOW
-		34, // COLOR_BLUE
-		36, // COLOR_CYAN
-		35, // COLOR_MAGENTA
-		0   // COLOR_WHITE
-	};
-
-	while( *msg )
-	{
-		if( Q_IsColorString( msg ) || *msg == '\n' )
-		{
-			// First empty the buffer
-			if( length > 0 )
-			{
-				buffer[ length ] = '\0';
-				fputs( buffer, stderr );
-				length = 0;
-			}
-
-			if( *msg == '\n' )
-			{
-				// Issue a reset and then the newline
-				fputs( "\033[0m\n", stderr );
-				msg++;
-			}
-			else
-			{
-				// Print the color code
-				Com_sprintf( buffer, sizeof( buffer ), "\033[%dm",
-						q3ToAnsi[ ColorIndex( *( msg + 1 ) ) ] );
-				fputs( buffer, stderr );
-				msg += 2;
-			}
-		}
-		else
-		{
-			if( length >= MAXPRINTMSG - 1 )
-				break;
-
-			buffer[ length ] = *msg;
-			length++;
-			msg++;
-		}
-	}
-
-	// Empty anything still left in the buffer
-	if( length > 0 )
-	{
-		buffer[ length ] = '\0';
-		fputs( buffer, stderr );
-	}
+void Sys_Init(void) {
+    Cmd_AddCommand("in_restart", Sys_In_Restart_f);
+    Cvar_Set("arch", OS_STRING " " ARCH_STRING);
+    Cvar_Set("username", Sys_GetCurrentUser());
 }
 
 /*
@@ -414,10 +214,9 @@ void Sys_AnsiColorPrint( const char *msg )
 Sys_Print
 =================
 */
-void Sys_Print( const char *msg )
-{
-	CON_LogWrite( msg );
-	CON_Print( msg );
+void Sys_Print(const char *msg) {
+    //CON_LogWrite(msg);
+    CON_Print(msg);
 }
 
 /*
@@ -425,18 +224,17 @@ void Sys_Print( const char *msg )
 Sys_Error
 =================
 */
-void Sys_Error( const char *error, ... )
-{
-	va_list argptr;
-	char    string[1024];
+void Sys_Error(const char *error, ...) {
+    va_list argptr;
+    char string[1024];
 
-	va_start (argptr,error);
-	Q_vsnprintf (string, sizeof(string), error, argptr);
-	va_end (argptr);
+    va_start (argptr, error);
+    Q_vsnprintf(string, sizeof(string), error, argptr);
+    va_end (argptr);
 
-	Sys_ErrorDialog( string );
+    Sys_ErrorDialog(string);
 
-	Sys_Exit( 3 );
+    Sys_Exit(3);
 }
 
 #if 0
@@ -447,14 +245,14 @@ Sys_Warn
 */
 static __attribute__ ((format (printf, 1, 2))) void Sys_Warn( char *warning, ... )
 {
-	va_list argptr;
-	char    string[1024];
+    va_list argptr;
+    char    string[1024];
 
-	va_start (argptr,warning);
-	Q_vsnprintf (string, sizeof(string), warning, argptr);
-	va_end (argptr);
+    va_start (argptr,warning);
+    Q_vsnprintf (string, sizeof(string), warning, argptr);
+    va_end (argptr);
 
-	CON_Print( va( "Warning: %s", string ) );
+    CON_Print( va( "Warning: %s", string ) );
 }
 #endif
 
@@ -465,14 +263,13 @@ Sys_FileTime
 returns -1 if not present
 ============
 */
-int Sys_FileTime( char *path )
-{
-	struct stat buf;
+int Sys_FileTime(char *path) {
+    struct stat buf;
 
-	if (stat (path,&buf) == -1)
-		return -1;
+    if (stat(path, &buf) == -1)
+        return -1;
 
-	return buf.st_mtime;
+    return buf.st_mtime;
 }
 
 /*
@@ -480,10 +277,9 @@ int Sys_FileTime( char *path )
 Sys_UnloadDll
 =================
 */
-void Sys_UnloadDll( void *dllHandle )
-{
-	Com_Printf("Sys_UnloadDll: not implemented\n");
-	return;
+void Sys_UnloadDll(void *dllHandle) {
+    Com_Printf("Sys_UnloadDll: not implemented\n");
+    return;
 }
 
 /*
@@ -495,10 +291,9 @@ from executable path, then fs_basepath.
 =================
 */
 
-void *Sys_LoadDll(const char *name, qboolean useSystemLib)
-{
-	Com_Printf("Sys_LoadDll: not implemented\n");
-	return NULL;
+void *Sys_LoadDll(const char *name, qboolean useSystemLib) {
+    Com_Printf("Sys_LoadDll: not implemented\n");
+    return NULL;
 }
 
 /*
@@ -509,11 +304,10 @@ Used to load a development dll instead of a virtual machine
 =================
 */
 void *Sys_LoadGameDll(const char *name,
-	intptr_t (QDECL **entryPoint)(int, ...),
-	intptr_t (*systemcalls)(intptr_t, ...))
-{
-	Com_Printf("Sys_LoadGameDll: not implemented\n");
-	return NULL;
+                      intptr_t (QDECL **entryPoint)(int, ...),
+                      intptr_t (*systemcalls)(intptr_t, ...)) {
+    Com_Printf("Sys_LoadGameDll: not implemented\n");
+    return NULL;
 }
 
 /*
@@ -521,22 +315,19 @@ void *Sys_LoadGameDll(const char *name,
 Sys_ParseArgs
 =================
 */
-void Sys_ParseArgs( int argc, char **argv )
-{
-	if( argc == 2 )
-	{
-		if( !strcmp( argv[1], "--version" ) ||
-				!strcmp( argv[1], "-v" ) )
-		{
-			const char* date = PRODUCT_DATE;
+void Sys_ParseArgs(int argc, char **argv) {
+    if (argc == 2) {
+        if (!strcmp(argv[1], "--version") ||
+            !strcmp(argv[1], "-v")) {
+            const char *date = PRODUCT_DATE;
 #ifdef DEDICATED
-			fprintf( stdout, Q3_VERSION " dedicated server (%s)\n", date );
+            fprintf( stdout, Q3_VERSION " dedicated server (%s)\n", date );
 #else
-			fprintf( stdout, Q3_VERSION " client (%s)\n", date );
+            fprintf(stdout, Q3_VERSION " client (%s)\n", date);
 #endif
-			Sys_Exit( 0 );
-		}
-	}
+            Sys_Exit(0);
+        }
+    }
 }
 
 #ifndef DEFAULT_BASEDIR
@@ -552,30 +343,26 @@ void Sys_ParseArgs( int argc, char **argv )
 Sys_SigHandler
 =================
 */
-void Sys_SigHandler( int signal )
-{
-	static qboolean signalcaught = qfalse;
+void Sys_SigHandler(int signal) {
+    static qboolean signalcaught = qfalse;
 
-	if( signalcaught )
-	{
-		fprintf( stderr, "DOUBLE SIGNAL FAULT: Received signal %d, exiting...\n",
-			signal );
-	}
-	else
-	{
-		signalcaught = qtrue;
-		VM_Forced_Unload_Start();
+    if (signalcaught) {
+        fprintf(stderr, "DOUBLE SIGNAL FAULT: Received signal %d, exiting...\n",
+                signal);
+    } else {
+        signalcaught = qtrue;
+        VM_Forced_Unload_Start();
 #ifndef DEDICATED
-		CL_Shutdown(va("Received signal %d", signal), qtrue, qtrue);
+        CL_Shutdown(va("Received signal %d", signal), qtrue, qtrue);
 #endif
-		SV_Shutdown(va("Received signal %d", signal) );
-		VM_Forced_Unload_Done();
-	}
+        SV_Shutdown(va("Received signal %d", signal));
+        VM_Forced_Unload_Done();
+    }
 
-	if( signal == SIGTERM || signal == SIGINT )
-		Sys_Exit( 1 );
-	else
-		Sys_Exit( 2 );
+    if (signal == SIGTERM || signal == SIGINT)
+        Sys_Exit(1);
+    else
+        Sys_Exit(2);
 }
 
 /*
@@ -583,87 +370,79 @@ void Sys_SigHandler( int signal )
 main
 =================
 */
-int main( int argc, char **argv )
-{
-	int   i;
-	char  commandLine[ MAX_STRING_CHARS ] = { 0 };
+int main(int argc, char **argv) {
+    int i;
+    char commandLine[MAX_STRING_CHARS] = {0};
 
-	extern void Sys_LaunchAutoupdater(int argc, char **argv);
-	Sys_LaunchAutoupdater(argc, argv);
+    //extern void Sys_LaunchAutoupdater(int argc, char **argv);
+    //Sys_LaunchAutoupdater(argc, argv);
 
 #ifndef DEDICATED
-	// SDL version check
+    // SDL version check
 
-	// Compile time
-#	if !SDL_VERSION_ATLEAST(MINSDL_MAJOR,MINSDL_MINOR,MINSDL_PATCH)
+    // Compile time
+#	if !SDL_VERSION_ATLEAST(MINSDL_MAJOR, MINSDL_MINOR, MINSDL_PATCH)
 #		error A more recent version of SDL is required
 #	endif
 
-	// Run time
-	SDL_version ver;
-	SDL_GetVersion( &ver );
+    // Run time
+    SDL_version ver;
+    SDL_GetVersion(&ver);
 
 #define MINSDL_VERSION \
-	XSTRING(MINSDL_MAJOR) "." \
-	XSTRING(MINSDL_MINOR) "." \
-	XSTRING(MINSDL_PATCH)
+    XSTRING(MINSDL_MAJOR) "." \
+    XSTRING(MINSDL_MINOR) "." \
+    XSTRING(MINSDL_PATCH)
 
-	if( SDL_VERSIONNUM( ver.major, ver.minor, ver.patch ) <
-			SDL_VERSIONNUM( MINSDL_MAJOR, MINSDL_MINOR, MINSDL_PATCH ) )
-	{
-		Sys_Dialog( DT_ERROR, va( "SDL version " MINSDL_VERSION " or greater is required, "
-			"but only version %d.%d.%d was found. You may be able to obtain a more recent copy "
-			"from http://www.libsdl.org/.", ver.major, ver.minor, ver.patch ), "SDL Library Too Old" );
+    if (SDL_VERSIONNUM(ver.major, ver.minor, ver.patch) <
+        SDL_VERSIONNUM(MINSDL_MAJOR, MINSDL_MINOR, MINSDL_PATCH)) {
+        Sys_Dialog(DT_ERROR, va("SDL version " MINSDL_VERSION " or greater is required, "
+                                        "but only version %d.%d.%d was found. You may be able to obtain a more recent copy "
+                                        "from http://www.libsdl.org/.", ver.major, ver.minor, ver.patch),
+                   "SDL Library Too Old");
 
-		Sys_Exit( 1 );
-	}
+        Sys_Exit(1);
+    }
 #endif
 
-	Sys_PlatformInit( );
+    Sys_PlatformInit();
 
-	// Set the initial time base
-	Sys_Milliseconds( );
+    // Set the initial time base
+    Sys_Milliseconds();
 
-#ifdef __APPLE__
-	// This is passed if we are launched by double-clicking
-	if ( argc >= 2 && Q_strncmp ( argv[1], "-psn", 4 ) == 0 )
-		argc = 1;
-#endif
+    //Sys_ParseArgs(argc, argv);
+    sceIoMkdir(DEFAULT_BASEDIR, 777);
+    Sys_SetBinaryPath(DEFAULT_BASEDIR);
+    Sys_SetDefaultInstallPath(DEFAULT_BASEDIR);
 
-	Sys_ParseArgs( argc, argv );
-	Sys_SetBinaryPath( Sys_Dirname( argv[ 0 ] ) );
-	Sys_SetDefaultInstallPath( DEFAULT_BASEDIR );
+    // Concatenate the command line for passing to Com_Init
+    for (i = 1; i < argc; i++) {
+        const qboolean containsSpaces = strchr(argv[i], ' ') != NULL;
+        if (containsSpaces)
+            Q_strcat(commandLine, sizeof(commandLine), "\"");
 
-	// Concatenate the command line for passing to Com_Init
-	for( i = 1; i < argc; i++ )
-	{
-		const qboolean containsSpaces = strchr(argv[i], ' ') != NULL;
-		if (containsSpaces)
-			Q_strcat( commandLine, sizeof( commandLine ), "\"" );
+        Q_strcat(commandLine, sizeof(commandLine), argv[i]);
 
-		Q_strcat( commandLine, sizeof( commandLine ), argv[ i ] );
+        if (containsSpaces)
+            Q_strcat(commandLine, sizeof(commandLine), "\"");
 
-		if (containsSpaces)
-			Q_strcat( commandLine, sizeof( commandLine ), "\"" );
+        Q_strcat(commandLine, sizeof(commandLine), " ");
+    }
 
-		Q_strcat( commandLine, sizeof( commandLine ), " " );
-	}
+    CON_Init();
+    Com_Init(commandLine);
+    NET_Init();
 
-	CON_Init( );
-	Com_Init( commandLine );
-	NET_Init( );
+    signal(SIGILL, Sys_SigHandler);
+    signal(SIGFPE, Sys_SigHandler);
+    signal(SIGSEGV, Sys_SigHandler);
+    signal(SIGTERM, Sys_SigHandler);
+    signal(SIGINT, Sys_SigHandler);
 
-	signal( SIGILL, Sys_SigHandler );
-	signal( SIGFPE, Sys_SigHandler );
-	signal( SIGSEGV, Sys_SigHandler );
-	signal( SIGTERM, Sys_SigHandler );
-	signal( SIGINT, Sys_SigHandler );
+    while (1) {
+        Com_Frame();
+    }
 
-	while( 1 )
-	{
-		Com_Frame( );
-	}
-
-	return 0;
+    return 0;
 }
 
--- code/psp2/sys_psp2.c
+++ code/psp2/sys_psp2.c
@@ -298,6 +298,7 @@ Sys_Mkdir
 qboolean Sys_Mkdir( const char *path )
 {
 	int result = sceIoMkdir(path, 0777);
+    printf("Sys_Mkdir: 0x%08X\n", result);
 
 	if( result != 0 )
 		return errno == EEXIST;
